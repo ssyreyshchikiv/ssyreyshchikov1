64be2ccd0c858b8438cd133b56b0bc64f7fa0858
ssyreyshchikov <ssyreyshchikov@mail.ru>
2018-04-13 20:43:56 +0300

    coursework top-wrapper build

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,63 @@
+// var gulp = require('gulp'); 
+// var browserSync = require('browser-sync').create();
+// var imagemin = require('gulp-imagemin');
+// var sass = require('gulp-sass');
+
+// gulp.task('default',['browser-sync'], function () {
+//     gulp.watch('site/**/*', ['build'])
+//     .on('change', browserSync.reload); //Refresh
+// });
+
+// //Сборка 'build':
+// gulp.task('build',['sass', 'images'], function () {
+//     return gulp.src('site/*.html')
+//            .pipe(gulp.dest('../public'));
+// });
+
+// //Препроцессор 'sass':
+// gulp.task('sass', function () {
+//   return gulp.src('site/sass/main.scss')
+//     .pipe(sass().on('error', sass.logError))
+//     .pipe(gulp.dest('../public/css'));
+// });
+
+// //Перенос и сжатие картинок 'images':
+// gulp.task('images', function () {
+//     return gulp.src('site/images/**/*')
+//            .pipe(gulp.dest('../public/images'));
+// });
+
+// //Задача 'Запуск сервера'('browser-sync'):
+// gulp.task('browser-sync', function() {
+//     browserSync.init({
+//         server: {
+//             baseDir: "../public"
+//         }
+//     });
+
+// });
+
 var gulp = require('gulp'); 
 var browserSync = require('browser-sync').create();
-var imagemin = require('gulp-imagemin');
-var sass = require('gulp-sass');
 
+//Задача умолчанию, перед которой выполнится 
+//задача 'Запуск сервера'('browser-sync'):
 gulp.task('default',['browser-sync'], function () {
     gulp.watch('site/**/*', ['build'])
     .on('change', browserSync.reload); //Refresh
 });
 
-//Сборка 'build':
-gulp.task('build',['sass', 'images'], function () {
-    return gulp.src('site/*.html')
+//Задача 'build':
+gulp.task('build', function () {
+    return gulp.src('site/**/*')
            .pipe(gulp.dest('../public'));
 });
 
-//Препроцессор 'sass':
-gulp.task('sass', function () {
-  return gulp.src('site/sass/main.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('../public/css'));
-});
-
-//Перенос и сжатие картинок 'images':
-gulp.task('images', function () {
-    return gulp.src('site/images/**/*')
-           .pipe(gulp.dest('../public/images'));
-});
-
-//Задача 'Запуск сервера'('browser-sync'):
+//Новая задача 'Запуск сервера'('browser-sync'):
 gulp.task('browser-sync', function() {
     browserSync.init({
         server: {
             baseDir: "../public"
         }
     });
-
 });
